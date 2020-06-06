e2c93ff7c5ce9da8b331ab3a8d27015b5535989b
Dmitry Zhuk <dimazhuk@gmail.com>
2020-06-06 21:50:56 +0300

    bug fixes

--- damask_pp/bproc_uni.sh
+++ damask_pp/bproc_uni.sh
@@ -1,3 +1,7 @@
+rm -rf avg_*
+rm -rf ebsd
+rm -rf mat_conf
+rm -rf postProc
 spfile=`find . -name '*.spectralOut'`
 echo $spfile
 export HDF5_DISABLE_VERSION_CHECK=3
@@ -10,3 +14,4 @@ python3 $DIR"/list_variables.py"
 source sp_runpp.sh
 python3 $DIR"/dam_avg.py"
 python3 $DIR"/dtpp.py"
+cd ..
--- damask_pp/list_variables.py
+++ damask_pp/list_variables.py
@@ -60,13 +60,13 @@ with open('sp_runpp.sh', 'w') as f_run:
     f_run.write("for f in *.txt; do\n  vtk_rectilinearGrid ./\"$f\"\ndone\n")
     f_run.write("for v in *.vtr; do\n  v2=\"${v:0:-14}.vtr\"\n  mv $v $v2\ndone\n")
     f_run.write("for v in *.vtr; do\n  vtk_addRectilinearGridData --vtk $v --data ")
-    res_vars = cr + co[:-1]
+    res_vars = cr + co
     for var in res_vars[:-1]:
         f_run.write("%s," % (var))
-    f_run.write("%s \"${v:0:-4}.txt\"\ndone\n" % (res_vars[-1]))
+    f_run.write("%s \"${v:0:-4}.txt\"\ndone\n" % (res_vars[-1])) 
     f_run.write("for f in *.txt; do\n  addDisplacement --nodal ./\"$f\"\ndone\n")
     f_run.write("for f in *nodal.txt; do\n  vtk_addRectilinearGridData --data 'fluct(f).pos','avg(f).pos'"
-                " --vtk \"${f:0:-10}.vtr\" $f \ndone\n")
+                " --vtk \"${f:0:-10}.vtr\" $f \ndone")
 f_run.close()
 
 print(crn)
